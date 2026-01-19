04eb2347c20e99098528fa0b22e87ef04ddc0966
WilliKahan <skaoi39@gmail.com>
2026-01-19 17:11:17 -0500

    se modifica page principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
+
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
